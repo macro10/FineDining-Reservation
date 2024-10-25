59d485dcf6e8cc85376744629c989a505ade3fc8
macro10 <mchaletrotter22@gmail.com>
2024-10-24 22:42:35 -0400

    replaced main with getRes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from get_res import getRes
 
-getRes(0, 5, "6", "30", "mtrotte1", "***REMOVED***", "7044515754", True)
+getRes(0, 5, "6", "30", "", "", "", True)
 # getRes(restaurant(int), pSize(int), hr(str), min(str), username(str), pswd(str), phone(str), complete(bool))
 
 # Prime is 0, Alo is 1, Kazoku is 2
