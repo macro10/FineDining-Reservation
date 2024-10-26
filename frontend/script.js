259b2ecbe1b5795496853cf43b9f3a2cc51e088c
macro10 <mchaletrotter22@gmail.com>
2024-10-26 15:34:14 -0400

    minor changes to config and reservation card buttons

--- frontend/script.js
+++ frontend/script.js
@@ -3,36 +3,36 @@ document.addEventListener('DOMContentLoaded', function() {
     const reservationCard = document.getElementById('ReservationCard');
     const closeReservationButton = document.getElementById('closeReservation');
     const configCard = document.getElementById('ConfigCard');
-    const closeConfigButton = document.getElementById('closeConfig');
-    const cancelConfigButton = document.getElementById('cancelConfig');
+    const submitConfigButton = document.getElementById('submitConfig');
+    const cancelConfigButton = document.getElementById('closeConfig');
     const showConfigButton = document.getElementById('showConfigButton');
 
+    // make reservation button shows reservation card
     refreshButton.addEventListener('click', function() {
         reservationCard.classList.toggle('hidden');
     });
 
+    // close reservation card
     closeReservationButton.addEventListener('click', function() {
         reservationCard.classList.add('hidden');
     });
 
-    // Function to hide the config card
+    // hide the config card
     function hideConfigCard() {
         configCard.classList.add('hidden');
     }
 
-    // Function to show the config card
+    // show the config card
     function showConfigCard() {
         configCard.classList.remove('hidden');
     }
 
+    submitConfigButton.addEventListener('click', function() {
+        // CONFIG SUBMISSION LOGIC
+        hideConfigCard()
+    });
+
     // Event listeners for close and cancel buttons
-    closeConfigButton.addEventListener('click', hideConfigCard);
     cancelConfigButton.addEventListener('click', hideConfigCard);
     showConfigButton.addEventListener('click', showConfigCard);
-
-    // You'll need to add logic to show the config card when needed
-    // For example:
-    // document.getElementById('showConfigButton').addEventListener('click', function() {
-    //     configCard.classList.remove('hidden');
-    // });
 });
